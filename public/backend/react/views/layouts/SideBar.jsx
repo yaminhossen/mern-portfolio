7aaa6771e9c14aca53ca2addc4ab8bcd1ee11979
Yamin Hossain <streatdot616@gmail.com>
2023-11-08 18:26:43 +0600

    sidebar re arrange

--- public/backend/react/views/layouts/SideBar.jsx
+++ public/backend/react/views/layouts/SideBar.jsx
@@ -9,153 +9,49 @@ function SideBar() {
                 <SingleLink to={'/'} text={'Dashboard'} icon={'monitoring'}></SingleLink>
 
                 <li>
-                    <DropDownLink text={'Users'} icon={'manage_accounts'}></DropDownLink>
+                    <DropDownLink text={'User Management'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
-                        <SingleLink to={'/user'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-                    </ul>
-                </li>
-
-                <li>
-                    <DropDownLink text={'User Roles'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-role'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-role/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user'} text={'users'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-role'} text={'user-roles'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-acheivement'} text={'user-acheivements'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-contact-number'} text={'user-contact-numbers'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-education'} text={'user-educations'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-email'} text={'user-emails'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-profile-info'} text={'user-profile-infos'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/user-social-link'} text={'user-social-links'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
-                <li>
-                    <DropDownLink text={'User Acheivement'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-acheivement'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-acheivement/create'} text={'create'} icon={'trip_origin'}></SingleLink>
 
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'User Contact Number'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-contact-number'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-contact-number/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'User Education'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-education'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-education/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'User Email'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-email'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-email/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'User Profile Info'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-profile-info'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-profile-info/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'User Social Link'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/user-social-link'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/user-social-link/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
+               
 
                 {/* blog link */}
                 <li>
                     <DropDownLink text={'Blog'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/blog'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/blog/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/blog-categorie'} text={'blog-categorie'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/blog-comment'} text={'blog-comment'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/blog-comment-reply'} text={'blog-comment-reply'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/blog-image'} text={'blog-image'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
-                <li>
-                    <DropDownLink text={'Blog Categories'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/blog-categorie'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/blog-categorie/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Blog Comments'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/blog-comment'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/blog-comment/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Blog Comment Replies'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/blog-comment-reply'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/blog-comment-reply/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Blog Images'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/blog-image'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/blog-image/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
+               
 
                 {/* content link */}
                 <li>
                     <DropDownLink text={'Content'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
-                        <SingleLink to={'/content'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/content/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/content'} text={'content'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/content-categorie'} text={'content-categorie'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/content-comment'} text={'content-comment'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/content-comment-reply'} text={'content-comment-reply'} icon={'trip_origin'}></SingleLink>
+                        <SingleLink to={'/content-image'} text={'content-image'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
-                <li>
-                    <DropDownLink text={'Content Categories'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/content-categorie'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/content-categorie/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Content Comments'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/content-comment'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/content-comment/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Content Comment Replies'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/content-comment-reply'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/content-comment-reply/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Content Images'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
-                        <SingleLink to={'/content-image'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/content-image/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
+               
 
                 {/* contact message */}
 
@@ -163,7 +59,6 @@ function SideBar() {
                     <DropDownLink text={'Contact Messages'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/contact-message'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/contact-message/create'} text={'create'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
@@ -174,7 +69,6 @@ function SideBar() {
                     <DropDownLink text={'Banners'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/banner'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/banner/create'} text={'create'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
@@ -185,15 +79,8 @@ function SideBar() {
                     <DropDownLink text={'Setting Titles'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/setting-title'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/setting-title/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Setting Values'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
                         <SingleLink to={'/setting-value'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/setting-value/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+
 
                     </ul>
                 </li>
@@ -203,70 +90,50 @@ function SideBar() {
                     <DropDownLink text={'Photo Gallery Categorie'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/photo-gallery-categorie'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/photo-gallery-categorie/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Photo Gallery Image'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
                         <SingleLink to={'/photo-gallery-image'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/photo-gallery-image/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+
 
                     </ul>
                 </li>
+              
 
                 {/* video gallery */}
                 <li>
                     <DropDownLink text={'Video Gallery Categorie'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/video-gallery-categorie'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/video-gallery-categorie/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Video Gallery Image'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
                         <SingleLink to={'/video-gallery-image'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/video-gallery-image/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+
 
                     </ul>
                 </li>
+                
 
                 {/* site url*/}
                 <li>
                     <DropDownLink text={'Site Url'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/site-url'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/site-url/create'} text={'create'} icon={'trip_origin'}></SingleLink>
-
-                    </ul>
-                </li>
-                <li>
-                    <DropDownLink text={'Site Url View Count'} icon={'manage_accounts'}></DropDownLink>
-                    <ul>
                         <SingleLink to={'/site-url-view-count'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/site-url-view-count/create'} text={'create'} icon={'trip_origin'}></SingleLink>
+
 
                     </ul>
                 </li>
-
+             
                 {/* tag*/}
                 <li>
                     <DropDownLink text={'Tag'} icon={'manage_accounts'}></DropDownLink>
                     <ul>
                         <SingleLink to={'/tag'} text={'all'} icon={'trip_origin'}></SingleLink>
-                        <SingleLink to={'/tag/create'} text={'create'} icon={'trip_origin'}></SingleLink>
 
                     </ul>
                 </li>
 
-                
+
 
                 {/* practice perpous */}
 
-                
+
             </ul>
         </nav>
     )
