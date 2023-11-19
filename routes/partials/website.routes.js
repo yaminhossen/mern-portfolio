8f6a77bce2c982f63b864dbface7ffdd831e61fd
Yamin Hossain <streatdot616@gmail.com>
2023-11-19 18:16:56 +0600

    all the templating complet

--- routes/partials/website.routes.js
+++ routes/partials/website.routes.js
@@ -22,11 +22,20 @@ router
 	.get("/about", function (req, res) {
 		return res.render("frontend/about");
 	})
+	.get("/contact", function (req, res) {
+		return res.render("frontend/contact");
+	})
 	.get("/contemporary", function (req, res) {
 		return res.render("frontend/contemporary");
 	})
+	.get("/contemporary-details", function (req, res) {
+		return res.render("frontend/contemporary_details");
+	})
 	.get("/social-work", function (req, res) {
 		return res.render("frontend/social_work");
+	})
+	.get("/social-work-details", function (req, res) {
+		return res.render("frontend/social_work_details");
 	})
 	.get("/book-review", function (req, res) {
 		return res.render("frontend/book_review");
@@ -34,8 +43,8 @@ router
 	.get("/blog-islam", function (req, res) {
 		return res.render("frontend/blog/islam");
 	})
-	.get("/blog-islami-movement", function (req, res) {
-		return res.render("frontend/blog/islami_movement");
+	.get("/blog-islamic-movement", function (req, res) {
+		return res.render("frontend/blog/islamic_movement");
 	})
 	.get("/blog-bangladesh", function (req, res) {
 		return res.render("frontend/blog/bangladesh");
@@ -48,6 +57,12 @@ router
 	})
 	.get("/blog-miscellaneous", function (req, res) {
 		return res.render("frontend/blog/miscellaneous");
+	})
+	.get("/photo-gallery", function (req, res) {
+		return res.render("frontend/gallery/photo_gallery");
+	})
+	.get("/video-gallery", function (req, res) {
+		return res.render("frontend/gallery/video_gallery");
 	})
 
 module.exports = () => router;
